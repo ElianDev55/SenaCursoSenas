97dfc50e90b5ffb8a0f6500bc9de717875c9420b
Joan Elian <jevillaurrutia@gmail.com>
2023-08-09 16:42:15 -0500

    Config all project back and add rows

---
+++ ApiSenaCursoSeñas/ApiSenaCursoSeñas/settings/production.py
@@ -0,0 +1,21 @@
+from .base import *
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_URL = 'static/'
