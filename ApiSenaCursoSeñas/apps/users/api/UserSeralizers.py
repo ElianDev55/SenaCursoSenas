8d96b26256bd835c9962ee40f68d6bc119a1da84
Joan Elian <jevillaurrutia@gmail.com>
2023-08-16 14:13:10 -0500

    arreglo

--- ApiSenaCursoSeñas/apps/users/api/UserSeralizers.py
+++
@@ -1,8 +0,0 @@
-from apps.users.models import User
-from rest_framework import serializers
-
-class UserSerializer(serializers.ModelSerializer):
-    
-    class Meta:
-        model = User
-        fields = '__all__'
