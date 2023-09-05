8f751ce117f2121e318fd9e71846c00338f80945
Joan Elian <jevillaurrutia@gmail.com>
2023-09-05 18:55:11 -0500

    Ayerbe x2

--- ApiSenaCursoSeñas/apps/videos/api/VideoSeralizers.py
+++ ApiSenaCursoSeñas/apps/videos/api/VideoSeralizers.py
@@ -2,7 +2,23 @@ from apps.videos.models import Video
 from rest_framework import serializers
 
 class VideoSerializer(serializers.ModelSerializer):
-    
+
     class Meta:
         model = Video
         fields = '__all__'
+
+
+class CategoryVideoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Video
+        fields = [
+            "video",
+            "title",
+            "description",
+            "state",
+            "miniature",
+            "created_at",
+            "updated_at",
+            "idUser",
+            "idCategory"
+        ]
--- ApiSenaCursoSeñas/apps/videos/api/views/CategoryVideoView.py
+++ ApiSenaCursoSeñas/apps/videos/api/views/CategoryVideoView.py
@@ -1,5 +1,5 @@
 from apps.videos.models import Video
-from apps.videos.api.VideoSeralizers import VideoSerializer
+from apps.videos.api.VideoSeralizers import CategoryVideoSerializer
 from rest_framework import generics
 from rest_framework import status
 from rest_framework import viewsets
@@ -11,7 +11,10 @@ from rest_framework.decorators import api_view
 def get_videos_by_category(request, category_id):
     try:
         videos = Video.objects.filter(idCategory=category_id, state=True)
-        serializer = VideoSerializer(videos, many=True)
+        
+        # Crear una instancia del serializador pasando la solicitud para obtener URLs completas
+        serializer = CategoryVideoSerializer(videos, many=True, context={'request': request})
+        
         return Response(serializer.data)
     except Exception as e:
         return Response({'error': str(e)}, status=500)
