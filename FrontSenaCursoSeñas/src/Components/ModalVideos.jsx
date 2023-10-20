96d8a4ac19756268148383210f01eff4440fb0d4
jevillaurrutia@gmail.com <jevillaurrutia@gmail.com>
2023-10-19 19:17:46 -0500

    Footer :p

--- FrontSenaCursoSeñas/src/Components/ModalVideos.jsx
+++ FrontSenaCursoSeñas/src/Components/ModalVideos.jsx
@@ -6,38 +6,54 @@ import '../Styles/ModalVideos.css'
 export function ModalVideos() {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [backdrop, setBackdrop] = React.useState('opaque');
+  const [size, setSize] = React.useState('md')
 
-  const backdrops = ["blur"];
 
-  const handleOpen = (backdrop) => {
+  const backdrops = "blur";
+  const sizes = "5xl";
+
+  const handleOpen = (backdrop,size) => {
     setBackdrop(backdrop);
+    setSize(size)
+    console.log(size);
     onOpen();
   };
+ 
+ 
 
   return (
     <>
       <div className="flex flex-wrap gap-3">
-        {backdrops.map((b) => (
+       
           <Button
-            key={b}
+          
+            key={backdrops}
             variant="flat"
             color="primary"
             variant="shadow"
-            onClick={() => handleOpen(b)}
+            onClick={() => handleOpen(backdrops,sizes)}
+            
             className="capitalize"
           >
             <AiFillEye />
           </Button>
-        ))}
+        
       </div>
       <Modal
-        maxwidth="90vw"
-        minwidth="80%"
+      size={size} 
         className="mx-auto"
         placement="center"
         backdrop={backdrop}
         isOpen={isOpen}
         onClose={onClose}
+        classNames={{
+          body: "py-6",
+          backdrop: "bg-[#292f46]/50 backdrop-opacity-40",
+          base: "border-[#FFFFFF] bg-[#FFFFFF] dark:bg-[#FFFFFF] text-[#000000]",
+          header: "border-b-[1px] border-[#000000]",
+          footer: "border-t-[1px] border-[#000000]",
+          closeButton: "hover:bg-white/5 active:bg-white/10",
+        }}
       >
         <ModalContent>
           {(onClose) => (
@@ -45,7 +61,7 @@ export function ModalVideos() {
               <ModalHeader className="flex flex-col gap-1">Pop</ModalHeader>
               <ModalBody>
                 <video
-                  src="https://v3.cdnpk.net/videvo-files/video/free/2017-05/watermarked/170111_064_Tokyo_IlluminatedStreet_1080p_preview.mp4"
+                  src="https://media.istockphoto.com/id/1371228411/es/v%C3%ADdeo/lapso-de-tiempo-de-la-noche-de-kabukicho-con-multitud-caminando-por-la-calle-en-la-ciudad-de.mp4?s=mp4-640x640-is&k=20&c=ZqSSHwj5JBAfo192hwy79b5eZl7AGGoXlqRwK1nFB5o="
                   controls
                   width="100%"
                   height="100%"
@@ -58,9 +74,6 @@ export function ModalVideos() {
                   onClick={onClose}
                 >
                   Close
-                </Button>
-                <Button color="primary" onPress={onClose}>
-                  Action
                 </Button>
               </ModalFooter>
             </>
--- FrontSenaCursoSeñas/src/Pages/App.jsx
+++ FrontSenaCursoSeñas/src/Pages/App.jsx
@@ -3,13 +3,17 @@
 import { AllInfo } from './AllInfo';
 import '../Styles/App.css'
 import { CategoryOfVideos } from './CategoryOfVideos';
+import { Footer } from '../Components/Footer';
+import { ModalVideos } from '../Components/ModalVideos';
+
 
 export const App =  () => {
   
   return (
     <>
-    
-    <CategoryOfVideos />
+    <ModalVideos/>
+    <CategoryOfVideos/>
+    <Footer/>
     
     </>
   )
