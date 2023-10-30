f52e39cd6fedd0b6f39041469924a8b3d295b057
jevillaurrutia@gmail.com <jevillaurrutia@gmail.com>
2023-10-30 15:34:21 -0500

    Delete all

--- FrontSenaCursoSeñas/src/Components/LayoutDetailCard.jsx
+++
@@ -1,9 +0,0 @@
-
-
-export const LayoutDetailCard = ({children}) => {
-    return (
-        <div className="gap-1 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 pt-5 justify-center">
-            {children}
-        </div>
-      );
-}
