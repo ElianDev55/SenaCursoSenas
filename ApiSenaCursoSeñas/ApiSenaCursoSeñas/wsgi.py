f32ad73e9d26b0d55d3b4c1c12b11abeeb0e3f67
jevillaurrutia@gmail.com <jevillaurrutia@gmail.com>
2023-11-20 18:33:42 -0500

    try deploy

--- ApiSenaCursoSeñas/ApiSenaCursoSeñas/wsgi.py
+++ ApiSenaCursoSeñas/ApiSenaCursoSeñas/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ApiSenaCursoSeñas.settings.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ApiSenaCursoSeñas.settings')
 
 application = get_wsgi_application()
