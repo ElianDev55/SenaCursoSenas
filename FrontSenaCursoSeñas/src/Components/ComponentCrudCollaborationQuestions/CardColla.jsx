ef3b65bcdd4e4d927598d6507fc4ce40da4e3992
jevillaurrutia@gmail.com <jevillaurrutia@gmail.com>
2023-11-28 08:43:51 -0500

    50% colaboracion hecha

--- FrontSenaCursoSeñas/src/Components/ComponentCrudCollaborationQuestions/CardColla.jsx
+++ FrontSenaCursoSeñas/src/Components/ComponentCrudCollaborationQuestions/CardColla.jsx
@@ -1,9 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
+import { Card, CardHeader, CardBody } from "@nextui-org/react";
+import { Select, SelectItem } from "@nextui-org/react";
+import { animals } from "./data";
 
-export const CardColla = ({ title }) => {
+export const CardColla = ({ id, title }) => {
+  const size = "sm";
+  const [selectedVideo, setSelectedVideo] = useState(null);
+  const [totalRating, setTotalRating] = useState(0);
+
+  const handleVideoSelection = (event) => {
+    setSelectedVideo(event.target.value);
+  };
+
+  const handleRating = (value) => {
+    setTotalRating(value);
+  };
+
   return (
-    <div className="flex flex-col items-center bg-white p-4 m-4 shadow-md">
-      <h2 className="text-lg font-bold mb-2">{title}</h2>
-    </div>
+    <Card className="py-4">
+      <div className="ml-4">
+        <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+          <h4 className="font-bold text-2xl">Formulario de colaboracion</h4>
+
+          {/* Componente Select para elegir el video */}
+          <Select
+            size={size}
+            label="Elije el video para calificar"
+            placeholder="Selecciona un video"
+            className="max-w-xs mt-3"
+            onChange={handleVideoSelection}
+            value={selectedVideo}
+          >
+            {animals.map((animal) => (
+              <SelectItem key={animal.value} value={animal.value}>
+                {animal.label}
+              </SelectItem>
+            ))}
+          </Select>
+        </CardHeader>
+
+        <CardBody className="overflow-visible py-2">
+          {/* Mostrar el video seleccionado o la imagen predeterminada */}
+          {selectedVideo ? (
+            <>
+              <video className="w-full mt-4" height="215" controls>
+                <source src={selectedVideo} type="video/mp4" />
+                Tu navegador no soporta el elemento de video.
+              </video>
+              <h4 className="font-bold text-xl mt-5">Formulario de colaboracion</h4>
+              <h4 className="font-bold text-base mt-5">¿ {title} ?</h4>
+              {/* Secciones adicionales para calificación */}
+              <div className="flex mt-2">
+                {[1, 2, 3, 4, 5].map((value) => (
+                  <span
+                    key={value}
+                    className={`star text-3xl cursor-pointer ${value <= totalRating ? "animate-bounce" : ""} ${
+                      value <= totalRating ? "text-yellow-500" : "text-gray-300"
+                    }`}
+                    onClick={() => handleRating(value)}
+                  >
+                    ★
+                  </span>
+                ))}
+              </div>
+              <p>Total de estrellas seleccionadas: {totalRating} </p>
+            </>
+          ) : (
+            <img
+              src="src/Assets/avisocolaboracion.png"
+              alt="Imagen predeterminada"
+              className="w-full  mt-4"
+            />
+          )}
+        </CardBody>
+      </div>
+    </Card>
   );
 };
--- FrontSenaCursoSeñas/src/Pages/CollaborationQuestions.jsx
+++ FrontSenaCursoSeñas/src/Pages/CollaborationQuestions.jsx
@@ -1,63 +1,39 @@
-// src/App.js
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Button } from "@nextui-org/react";
 import { CardColla } from "../Components/ComponentCrudCollaborationQuestions/CardColla";
-import axios from "axios";
+import { CollaborationQuestionsContext } from "../Context/ContextCollaborationQuestions";
+import { GrLinkNext } from "react-icons/gr";
 
 export function CollaborationQuestions() {
+  const context = useContext(CollaborationQuestionsContext);
+  const data = context.collaborationQuestions || [];
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [questions, setQuestions] = useState([]);
 
-  const handleNextQuestion = () => {
-    // Verificar si hay preguntas disponibles
-    if (questions.length > 0) {
-      const nextIndex = (currentIndex + 1) % questions.length;
-      setCurrentIndex(nextIndex);
-    }
+
+  // Utiliza useMemo para calcular apiData solo cuando cambie data o currentIndex
+  const apiData = React.useMemo(() => {
+    return [
+      { id: 1, title: data[0]?.QuestionOne   },
+      { id: 2, title: data[0]?.QuestionTwo },
+      { id: 3, title: data[0]?.QuestionThree },
+      { id: 4, title: data[0]?.QuestionFour },
+      { id: 5, title: data[0]?.QuestionFive },
+    ];
+  }, [data, currentIndex]);
+
+  const currentCard = apiData[currentIndex];
+
+  const handleNextCard = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % apiData.length);
   };
 
-  useEffect(() => {
-    // Obtener datos de la API
-    axios
-      .get("http://127.0.0.1:8000/CollaborationQuestions/")
-      .then((response) => {
-        const data = response.data;
-
-        // Sobrescribir todas las propiedades 'title' en apiData
-        const transformedQuestions = data.map((questionData, index) => ({
-          id: index + 1, // Asumiendo que el índice base es 1 (como en apiData)
-          title: questionData.QuestionOne,
-        }));
-
-        setQuestions(transformedQuestions);
-        setCurrentIndex(0); // Iniciar desde la primera pregunta
-      })
-      .catch((error) => {
-        console.error("Error al obtener datos de la API", error);
-      });
-  }, []);
-
   return (
-    <div className="flex flex-col md:flex-row mt-8">
-      {/* Video en la izquierda (arriba en dispositivos pequeños) */}
-      <div className="md:w-1/2 md:pr-4">
-        {/* Aquí puedes insertar tu componente de video local */}
-        <video className="w-full" height="315" controls>
-          <source src="http://127.0.0.1:8000/media/videos/bogota.mp4" type="video/mp4" />
-          Tu navegador no soporta el elemento de video.
-        </video>
-      </div>
-
-      {/* Tarjeta a la derecha (abajo en dispositivos pequeños) */}
-      <div className="md:w-1/2 md:pl-4">
-        {questions.length > 0 && (
-          <>
-            <CardColla {...questions[currentIndex]} />
-            <Button variant="contained" color="success" onClick={handleNextQuestion}>
-              Siguiente
-            </Button>
-          </>
-        )}
+    <div className="flex flex-col md:flex-row mt-4 justify-center items-center">
+      <div className="md:w-1/2 md:pl-4 flex flex-col items-end">
+        <CardColla {...currentCard} />
+        <Button color="success" onClick={handleNextCard}>
+          <GrLinkNext className="mr-2 text-green-500 text-lg" />
+        </Button>
       </div>
     </div>
   );
