ff52885a3492efe8ad6518693557583f0c6aa77a
jevillaurrutia@gmail.com <jevillaurrutia@gmail.com>
2023-12-04 23:56:16 -0500

    MMM

--- FrontSenaCursoSeñas/src/Components/ComponentCrudCollaborationQuestions/CardColla.jsx
+++ FrontSenaCursoSeñas/src/Components/ComponentCrudCollaborationQuestions/CardColla.jsx
@@ -1,20 +1,61 @@
+// CardColla.jsx
 import React, { useState, useEffect } from "react";
 import { Card, CardHeader, CardBody } from "@nextui-org/react";
 import { Select, SelectItem } from "@nextui-org/react";
-import { MdOutlineSendAndArchive } from "react-icons/md";
 import { Button } from "@nextui-org/react";
-import { Tooltip } from "@nextui-org/react";
+import { MdOutlineSendAndArchive } from "react-icons/md";
 
-
-export const CardColla = ({ id, title }) => {
+export const CardColla = (props) => {
+  const { id, title, onSave, onVideoChange, showSubmitButton, onSubmitButtonClick, totalRating, setTotalRating, currentIndex } = props;
   const size = "sm";
   const [selectedVideoUrl, setSelectedVideoUrl] = useState(null);
-  const [totalRating, setTotalRating] = useState(0);
   const [videos, setVideos] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [responseSubmitted, setResponseSubmitted] = useState(false);
 
   const handleRating = (value) => {
     setTotalRating(value);
+  };
+
+  const handleSave = () => {
+    if (selectedVideoUrl && totalRating > 0) {
+      onSave({ id, stars: totalRating });
+
+      // Reiniciar el estado de las estrellas después de guardar
+      setTotalRating(0);
+
+      // Marcar que se ha enviado una respuesta
+      setResponseSubmitted(true);
+    }
+
+    // Verificar si es la última pregunta y mostrar el botón de "Subir respuestas"
+    if (onSubmitButtonClick && currentIndex + 1 === 5) {
+      onSubmitButtonClick();
+    }
+  };
+
+  const handleVideoSelection = (event) => {
+    // Si ya se ha enviado una respuesta, no permitir seleccionar otro video
+    if (responseSubmitted) {
+      console.log("Ya se ha enviado una respuesta. No se puede seleccionar otro video.");
+      return;
+    }
+
+    const videoUrl = event.target.value;
+
+    // Reinicia el índice de preguntas al cambiar de video
+    onVideoChange();
+
+    // Realizar la solicitud a la API para obtener información adicional si es necesario
+    fetch(videoUrl)
+      .then((response) => response.text())
+      .then((data) => {
+        console.log(data);
+        setSelectedVideoUrl(videoUrl);
+      })
+      .catch((error) => {
+        console.error("Error fetching selected video:", error);
+      });
   };
 
   useEffect(() => {
@@ -32,29 +73,12 @@ export const CardColla = ({ id, title }) => {
       });
   }, []);
 
-  const handleVideoSelection = (event) => {
-    const videoUrl = event.target.value;
-  
-    // Realizar la solicitud a la API para obtener información adicional si es necesario
-    fetch(videoUrl)
-      .then((response) => response.text())  // Utiliza response.text() en lugar de response.json()
-      .then((data) => {
-        console.log(data);  // Imprime el contenido de la respuesta
-        setSelectedVideoUrl(videoUrl);
-      })
-      .catch((error) => {
-        console.error("Error fetching selected video:", error);
-      });
-  };
-  
-
   return (
     <Card className="py-4">
       <div className="ml-4">
         <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
           <h4 className="font-bold text-2xl">Formulario de colaboracion</h4>
 
-          {/* Componente Select para elegir el video */}
           <Select
             size={size}
             label="Elije el video para calificar"
@@ -76,7 +100,7 @@ export const CardColla = ({ id, title }) => {
             <p>Cargando videos...</p>
           ) : (
             <>
-              {selectedVideoUrl ? (
+              {selectedVideoUrl && !showSubmitButton ? (
                 <>
                   <video className="w-full mt-4" height="215" controls>
                     <source src={selectedVideoUrl} type="video/mp4" />
@@ -84,12 +108,13 @@ export const CardColla = ({ id, title }) => {
                   </video>
                   <h4 className="font-bold text-xl mt-5">Formulario de colaboracion</h4>
                   <h4 className="font-bold text-base mt-5">¿ {title} ?</h4>
-                  {/* Secciones adicionales para calificación */}
                   <div className="flex mt-2">
                     {[1, 2, 3, 4, 5].map((value) => (
                       <span
                         key={value}
-                        className={`star text-3xl cursor-pointer ${value <= totalRating ? "animate-bounce" : ""} ${
+                        className={`star text-3xl cursor-pointer ${
+                          value <= totalRating ? "animate-bounce" : ""
+                        } ${
                           value <= totalRating ? "text-yellow-500" : "text-gray-300"
                         }`}
                         onClick={() => handleRating(value)}
@@ -97,11 +122,9 @@ export const CardColla = ({ id, title }) => {
                         ★
                       </span>
                     ))}
-                   <Tooltip title="Enviar estrellas" arrow>
-      <Button color="">
-        <MdOutlineSendAndArchive className="text-lg hover:text-green-500" />
-      </Button>
-    </Tooltip>
+                    <Button color="" onClick={handleSave}>
+                      <MdOutlineSendAndArchive className="text-lg hover:text-green-500" />
+                    </Button>
                   </div>
                   <p>Total de estrellas seleccionadas: {totalRating} </p>
                 </>
@@ -111,6 +134,14 @@ export const CardColla = ({ id, title }) => {
                   alt="Imagen predeterminada"
                   className="w-full  mt-4"
                 />
+              )}
+              {/* Mostrar el botón de "Subir respuestas" después de pasar la última pregunta */}
+              {showSubmitButton && currentIndex + 1 === 5 && (
+                <div className="flex justify-center mt-4">
+                  <Button color="success" onClick={onSubmitButtonClick}>
+                    Subir respuestas
+                  </Button>
+                </div>
               )}
             </>
           )}
--- FrontSenaCursoSeñas/src/Pages/CollaborationQuestions.jsx
+++ FrontSenaCursoSeñas/src/Pages/CollaborationQuestions.jsx
@@ -1,20 +1,20 @@
+// CollaborationQuestions.jsx
 import React, { useState, useEffect, useContext } from "react";
 import { Button } from "@nextui-org/react";
 import { CardColla } from "../Components/ComponentCrudCollaborationQuestions/CardColla";
 import { CollaborationQuestionsContext } from "../Context/ContextCollaborationQuestions";
 import { GrLinkNext } from "react-icons/gr";
-import 'animate.css'; 
+import 'animate.css';
 
 export function CollaborationQuestions() {
   const context = useContext(CollaborationQuestionsContext);
   const data = context.collaborationQuestions || [];
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [starData, setStarData] = useState([]);
 
-
-  // Utiliza useMemo para calcular apiData solo cuando cambie data o currentIndex
   const apiData = React.useMemo(() => {
     return [
-      { id: 1, title: data[0]?.QuestionOne   },
+      { id: 1, title: data[0]?.QuestionOne },
       { id: 2, title: data[0]?.QuestionTwo },
       { id: 3, title: data[0]?.QuestionThree },
       { id: 4, title: data[0]?.QuestionFour },
@@ -23,20 +23,53 @@ export function CollaborationQuestions() {
   }, [data, currentIndex]);
 
   const currentCard = apiData[currentIndex];
+  const [totalRating, setTotalRating] = useState(0);
 
   const handleNextCard = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % apiData.length);
+    if (currentIndex + 1 < apiData.length) {
+      setCurrentIndex((prevIndex) => prevIndex + 1);
+    } else {
+      console.log("Ya has respondido todas las preguntas.");
+    }
+  };
+
+  const handleSaveResponse = ({ id, stars }) => {
+    setStarData((prevStarData) => [
+      ...prevStarData,
+      { id, stars }
+    ]);
+  };
+
+  const handleVideoChange = () => {
+    setStarData([]);
+  };
+
+  const handleSubirRespuestas = () => {
+    console.log("Datos de estrellas guardados:", starData);
   };
 
   return (
     <div className="flex flex-col md:flex-row mt-4 justify-center items-center">
       <div className="md:w-1/2 md:pl-4 flex flex-col items-center md:items-end">
-        <CardColla {...currentCard} />
+        <CardColla
+          {...currentCard}
+          apiData={apiData}  // Agregado para pasar apiData como prop
+          onSave={(params) => {
+            handleSaveResponse(params);
+            // Reiniciar el estado de las estrellas después de guardar
+            setTotalRating(0);
+          }}
+          onVideoChange={handleVideoChange}
+          showSubmitButton={!apiData[currentIndex + 1]}
+          onSubmitButtonClick={handleSubirRespuestas}
+          totalRating={totalRating}
+          setTotalRating={setTotalRating}
+        />
       </div>
       <div className="mt-4 md:mt-0 md:pl-4 flex justify-center md:justify-end">
-      <Button color="" onClick={handleNextCard} className="animated rotateIn">
-      <GrLinkNext className="mr-2 text-green-500 text-lg" />
-    </Button>
+        <Button color="" onClick={handleNextCard} className="animated rotateIn">
+          <GrLinkNext className="mr-2 text-green-500 text-lg" />
+        </Button>
       </div>
     </div>
   );
